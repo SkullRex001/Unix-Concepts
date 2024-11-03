97e8ec1e3f620bee3526e81cd370aa89eabacae5
SkullRex001 <honestaditya@gmail.com>
2024-11-03 06:10:35 +0530

    day2

---
+++ bashScripting/variables.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+
+#don't use space
+myname="Aditya Vikram Singh"
+
+myage="23"
+
+echo "My name is $myname and I am $myage years old."
+
+echo 'My name is $myname and I am $myage years old.'
+
+# after running the, i can't access myname is terminal, why?
+
+
---
+++ bashScripting/whichShell.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/zsh
+
+
+#zsh myscript.sh will ignore shebang and run script using zsh
+
+# chmod +x myscript.sh and then run it with ./myscript.sh will run script accoring to shebang
+
+
+
+#which shell I am using
+
+echo $SHELL
+
+
+#what to type to run bash
+
+which bash
+
+
+#what to type to run zsh
+
+which zsh
+
+
+#make this file execuitable
+
+# sudo chmod +x whichShell.sh
+
+# to execute a script just enter the path in shell
